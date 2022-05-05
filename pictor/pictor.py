2e88554fdcc915412e41e6ea4f1a57ee85f7c12b
William <luo.wei@zju.edu.cn>
2022-05-05 17:23:40 +0800

    [M] added objects and plotters

--- pictor/pictor.py
+++ pictor/pictor.py
@@ -13,22 +13,72 @@
 # limitations under the License.
 # ====-==================================================================-======
 from roma import Easel
+from typing import Callable
 
 
 
 class Pictor(Easel):
 
+  class Keys:
+    OBJECTS = 'ObJeCtS'
+    PLOTTERS = 'PlOtTeRs'
+
   def __init__(self, title='Pictor'):
     # Call parent's constructor
     super(Pictor, self).__init__()
 
+    # Set window title
+    self.static_title = title
+
+    # Create dimensions for objects and layers
+    self.create_dimension(self.Keys.OBJECTS)
+    self.create_dimension(self.Keys.PLOTTERS)
+
+    # Register common events
+    self._register_key_events()
+
+  # region: Properties
+
+  @property
+  def objects(self): return self.axes[self.Keys.OBJECTS]
+
+  @objects.setter
+  def objects(self, value):
+    self.set_to_axis(self.Keys.OBJECTS, value, overwrite=True)
+
+  # endregion: Properties
+
+  # region: Private Methods
+
+  def _register_key_events(self):
+    pass
+
+  # endregion: Private Methods
+
+  # region: Public Methods
+
+  def add_plotter(self, plotter: Callable, index: int = -1):
+    if not callable(plotter):
+      raise ValueError('!! A plotter should be callable')
+    self.add_to_axis(self.Keys.PLOTTERS, plotter, index=index)
+
+  def refresh(self):
+    self.title = f'{self.cursor_string} {self.static_title}'
+
+  # endregion: Public Methods
+
+  # region: Builtin Commands
 
   def yell(self, text):
+    """Doc string ..."""
     self.title = text
+
+  # endregion: Builtin Commands
 
 
 
 if __name__ == '__main__':
   p = Pictor()
+  p.objects = [1, 2, 3]
   p.show()
 
