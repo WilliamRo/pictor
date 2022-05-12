0b8efcceb1b71614f92b907f11c3d361e36cd7d1
Oliver <22160245.zju.edu.cn>
2022-05-12 14:58:40 +0800

    [M] updated __init__

--- pictor/__init__.py
+++ pictor/__init__.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ======-=========================================================-=============
-from pictor.pictor import Pictor
+from .pictor import Pictor
 
 # ------------------------------------------------------------------------------
 # Deprecated Objects
 # ------------------------------------------------------------------------------
-from pictor.vinci.vinci import DaVinci
+from .vinci.vinci import DaVinci
