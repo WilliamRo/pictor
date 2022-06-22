925977108c7fe28ba2a75d1516219d8eff32d868
WilliamRo <walienluo@gmail.com>
2022-06-22 20:40:20 +0800

    [M] modified doc string

--- pictor/plugins/database.py
+++ pictor/plugins/database.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===-=====================================================================-====
+"""This class provides filter functions for subclasses of Pictor"""
 from roma import Nomear
 
 import inspect
